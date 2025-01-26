5481b283253b2e17f4bc8b8380e4a8215f83c826
Zeynal <zey.verdiyev@gmail.com>
2025-01-27 02:12:02 +0400

    updated last small fixes

--- app/app.js
+++ app/app.js
@@ -1,26 +1,26 @@
 class App {
-  canvas; // Canvas
+  canvas;
   html;
   modeManager = new ModeManager();
-  ui_mode; // black / white
   graph = new Graph();
 
   constructor(htmlCanvas) {
     this.html = htmlCanvas;
     this.canvas = new Canvas(htmlCanvas);
 
-    const examples = this.generateWalls();
-    examples.forEach((n) => {
-      this.canvas.walls.push(n);
-    });
-
-    this.modeManager.setMode(new WallMode(this.canvas, this.graph));
     this.initListenes();
   }
 
   initListenes() {
+    const startButton = document.querySelector("#startButton");
+    if (startButton) {
+      startButton.addEventListener("click", () => {
+        this.modeManager.setMode(new WallMode(this.canvas, this.graph));
+      });
+    }
+
     this.html.addEventListener("click", (event) => {
-      this.modeManager.currentMode.onClick(event);
+      this.modeManager.onClick(event);
 
       if (this.canvas.roomDetected) {
         this.modeManager.setMode(new FurnitureMode(this.canvas, this.graph));
@@ -28,35 +28,15 @@ class App {
     });
 
     this.html.addEventListener("mousemove", (event) => {
-      this.modeManager.currentMode.onMove(event);
+      this.modeManager.onMove(event);
     });
 
     window.addEventListener("keydown", (event) => {
-      this.modeManager.currentMode.onKeyDown(event);
+      this.modeManager.onKeyDown(event);
     });
   }
 
   run() {
     this.canvas.animate();
-  }
-
-  generateWalls() {
-    const ret = [];
-
-    // {
-    //   const newWall = new Wall();
-    //   newWall.start = { x: 100, y: 100 };
-    //   newWall.end = { x: 300, y: 100 };
-    //   ret.push(newWall);
-    // }
-
-    // {
-    //   const newWall = new Wall();
-    //   newWall.start.position = {x: 10, y: 10};
-    //   newWall.end.position = {x: 50, y: 50};
-    //   ret.push(newWall);
-    // }
-
-    return ret;
   }
 }
--- app/modeState/ModeManager.js
+++ app/modeState/ModeManager.js
@@ -1,19 +1,19 @@
 class ModeManager {
-  currentMode = new WallMode();
+  currentMode = null;
 
   setMode(mode) {
     this.currentMode = mode;
   }
 
   onClick(event) {
-    this.currentMode.onClick(event);
+    this.currentMode?.onClick(event);
   }
 
   onMove(event) {
-    this.currentMode.onMove(event);
+    this.currentMode?.onMove(event);
   }
 
   onKeyDown(event) {
-    this.currentMode.onKeyDown(event);
+    this.currentMode?.onKeyDown(event);
   }
 }
